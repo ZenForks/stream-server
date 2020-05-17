874dbf76417349f3fe482adb1d043b7ce1196f56
KurokuTetsuya <perumnas.good@gmail.com>
2020-05-17 22:57:08 +0700

    Improvement

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,33 @@ import { directory, port } from "./config.json";
 import { resolve, join } from "path";
 
 const app = express();
+console.log(join(__dirname, "assets"))
+app.use(express.static(join(__dirname, "assets")));
 app.set("view engine", "ejs");
 app.set("views", join(__dirname, "views"));
 app.listen(port, () => {
    console.log(`Your app is listening on ${port}`);
 });
 
+const toTitle = (str: string): string => {
+   const firstChar = str.split("")[0].toUpperCase();
+   return firstChar + str.slice(1);
+}
 app.get("/", (req: any, res: any) => {
-   return res.redirect("/default");
+   return res.status(200).render("home.ejs", {
+      udir: toTitle(directory)
+   });
 });
 
 app.get("/:directory", (req: any, res: any) => {
    const udir = req.params.directory === "default" ? "" : req.params.directory ? req.params.directory : "";
-   console.log(resolve(__dirname, directory, udir))
    fs.readdir(resolve(__dirname, directory, udir), (err: any, files: string[]) => {
       if (err) {
          return res.status(404).send(JSON.parse(JSON.stringify({ "error": err })));
       }
       res.status(200).render("directory.ejs", {
-         files, udir: udir === "" ? directory : udir
+         udir: toTitle(udir === "" ? directory : udir),
+         files
       });
    });
 });
