71e4d3491a8437d9f42fab60f431a3daacc2db25
KurokuTetsuya <perumnas.good@gmail.com>
2020-05-17 21:31:13 +0700

    We've done with this (?)

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import fs from "fs";
+import { directory, port } from "./config.json";
+import { resolve, join } from "path";
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", join(__dirname, "views"));
+app.listen(port, () => {
+   console.log(`Your app is listening on ${port}`);
+});
+
+app.get("/", (req: any, res: any) => {
+   return res.redirect("/default");
+});
+
+app.get("/:directory", (req: any, res: any) => {
+   const udir = req.params.directory === "default" ? "" : req.params.directory ? req.params.directory : "";
+   console.log(resolve(__dirname, directory, udir))
+   fs.readdir(resolve(__dirname, directory, udir), (err: any, files: string[]) => {
+      if (err) {
+         return res.status(404).send(JSON.parse(JSON.stringify({ "error": err })));
+      }
+      res.status(200).render("directory.ejs", {
+         files, udir: udir === "" ? directory : udir
+      });
+   });
+});
+
+app.get("/watch/:name", (req, res) => {
+   if (!req.params.name) return res.sendStatus(404);
+   const name = req.params.name;
+   fs.createReadStream(`${resolve(__dirname, directory)}/${name}`)
+       .pipe(res);
+   res.status(200).send("<h1> OK </h1>");
+});
